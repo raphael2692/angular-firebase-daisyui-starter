60390d0a7ca733334333d8056260985a1ddbd873
Raffaele Spataro <rspataro@softstrategy.it>
2024-05-21 16:58:09 +0200

    remove settings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,8 @@ import { Routes } from '@angular/router';
 
 import { ProfileComponent } from './components/profile/profile.component';
 import { LoginComponent } from './components/login/login.component';
-import { SettingsComponent } from './components/settings/settings.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent },
-    {path: 'settings', component: SettingsComponent },
     {path: 'profile', component: ProfileComponent}
 ];
